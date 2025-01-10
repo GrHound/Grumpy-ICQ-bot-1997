bdce8482b602b1ad737ee2ac4500bfd4d41966f7
GrHound <36005587+GrHound@users.noreply.github.com>
2025-01-10 15:54:23 +0100

    Delete splicq/lang.h

--- splicq/lang.h
+++
@@ -1,9 +0,0 @@
-#ifdef RUSSIAN_LANG
-#include "russian.h"
-#elif defined ENGLISH_LANG
-#include "english.h"
-#elif defined BRAZIL_LANG
-#include "brazil.h"
-#else
-#error "Must select a language."
-#endif
